3679857505a89a6ce56621f0cfe737f850940c4b
Apoorv <apoorv13103663@gmail.com>
2020-04-12 10:49:17 +0530

    Master Commit
    Signed-off-by: Apoorv <apoorv13103663@gmail.com>

---
+++ Compression.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Rainier.Helper
+{
+    public static class TKSCompression
+    {
+        private const int CHUNK_SIZE = 10000;
+
+        public static string CompressDataTableToString(DataTable orgTable)
+        {
+            int chunks = 0;
+            int chunkLimit = 0;
+            int rowCount = orgTable.Rows.Count;
+
+            var newDt = new DataTable();
+            newDt = orgTable.Clone();
+            newDt.Clear();
+
+            List<DataTable> chunkedTableList = new List<DataTable>();
+            string finalContentString = "";
+
+            //Separate DataTable into chunks of DataTable
+            foreach (DataRow row in orgTable.Rows)
+            {
+                DataRow newRow = newDt.NewRow();
+                newRow.ItemArray = row.ItemArray;
+                newDt.Rows.Add(newRow);
+                chunkLimit++;
+                if (chunkLimit == CHUNK_SIZE || (chunks == rowCount / CHUNK_SIZE && chunkLimit == rowCount % CHUNK_SIZE))
+                {
+                    chunkedTableList.Add(newDt);
+                    chunks++;
+                    newDt = new DataTable();
+                    newDt = orgTable.Clone();
+                    newDt.Clear();
+                    chunkLimit = 0;
+                }
+            }
+
+            //Compress each DataTable into binary and add to content string
+            foreach (DataTable table in chunkedTableList)
+            {
+                byte[] binaryDataResult = null;
+                using (MemoryStream memStream = new MemoryStream())
+                {
+                    using (GZipStream gzip = new GZipStream(memStream,
+                    CompressionMode.Compress, true))
+                    {
+                        var formatter = new BinaryFormatter();
+                        table.RemotingFormat = SerializationFormat.Binary;
+                        formatter.Serialize(gzip, table);
+                    }
+                    binaryDataResult = memStream.ToArray();
+                }
+                using (MemoryStream memStream = new MemoryStream())
+                {
+                    using (GZipStream gzip = new GZipStream(memStream,
+                    CompressionMode.Compress, true))
+                    {
+                        gzip.Write(binaryDataResult, 0, binaryDataResult.Length);
+                    }
+                    binaryDataResult = memStream.ToArray();
+ 
+                }
+                string chunkedContent = System.Convert.ToBase64String(binaryDataResult) + ';';
+                finalContentString += chunkedContent;
+            }
+            finalContentString = finalContentString.Remove(finalContentString.Length - 1);
+            return finalContentString;
+        }
+
+        public static DataTable DecompressStringToDataTable(string compressedString)
+        {
+            DataSet tempDataSet = new DataSet();
+            var splitDataTables = compressedString.Split(';');
+
+            //Decompress and add to DataSet
+            foreach (var tableString in splitDataTables)
+            {
+                DataTable dt = new DataTable();
+                byte[] convertedString = System.Convert.FromBase64String(tableString);
+                using (MemoryStream stream = new MemoryStream(convertedString))
+                {
+                    using (GZipStream gzip = new GZipStream(stream, CompressionMode.Decompress))
+                    {
+                        using (var outputStream = new MemoryStream())
+                        {
+                            gzip.CopyTo(outputStream);
+                            convertedString = outputStream.ToArray();
+                        }
+                    }
+                }
+                using (MemoryStream stream = new MemoryStream(convertedString))
+                {
+                    using (GZipStream gzip = new GZipStream(stream, CompressionMode.Decompress))
+                    {
+                        BinaryFormatter bformatter = new BinaryFormatter();
+                        dt = (DataTable)bformatter.Deserialize(gzip);
+                    }
+                }
+                tempDataSet.Merge(dt);
+            }
+            return tempDataSet.Tables[0];
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # GZip-Compression-Decompression
 Double Compression and Decompression algorithm 
+
+This algorithm can convert DataTables of very large sizes(in order of GBs) into byte strings of order in ranges of 100-300 times less than the original size.
+Actual compression ratio depends on System fctors also like RAM, maximum variable size e.t.c.
+
+It breaks down the DataTable into chunks of tables and double compresses each table into a byte string which is concatenated and returned.
+
+Similarly, while decompression, the string separated by ',' into list of strings which is double decompressed to generate each chunk table. 
+These all are then combined to return the actual table.
+
+Feel free to raise an MR to improve the efficiency of the algorithm.
+
